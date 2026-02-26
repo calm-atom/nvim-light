210b09be3b707f266722f9260d112514e808cefb
John Buckley <jbuckleyv@gmail.com>
2026-02-26 12:46:10 -0600

    feat: add LSP support

--- init.lua
+++ init.lua
@@ -126,7 +126,7 @@ MiniDeps.setup({
 })
 
 MiniDeps.add('neovim/nvim-lspconfig')
-MiniDeps.add('oskarnurm/koda.nvim')
+MiniDeps.add('stevearc/conform.nvim')
 MiniDeps.add({
   source = 'zenbones-theme/zenbones.nvim',
   depends = { 'rktjmp/lush.nvim' },
@@ -140,3 +140,115 @@ MiniDeps.add({
 -- ==                         PLUGIN CONFIGURATION                         == --
 -- ========================================================================== --
 vim.cmd.colorscheme('rosebones')
+
+-- See :help MiniSurround.config
+require('mini.surround').setup({})
+
+-- See :help MiniNotify.config
+require('mini.notify').setup({
+  lsp_progress = {enable = false},
+})
+
+-- See :help MiniFiles.config
+local mini_files = require('mini.files')
+mini_files.setup({})
+
+-- Toggle file explorer
+-- See :help MiniFiles-navigation
+vim.keymap.set('n', '<leader>e', function()
+  if mini_files.close() then
+    return
+  end
+
+  mini_files.open()
+end, {desc = 'File explorer'})
+
+-- See :help MiniPick.config
+require('mini.pick').setup({})
+
+-- See available pickers
+-- :help MiniPick.builtin
+-- :help MiniExtra.pickers
+vim.keymap.set('n', '<leader>?', '<cmd>Pick oldfiles<cr>', {desc = 'Search file history'})
+vim.keymap.set('n', '<leader><space>', '<cmd>Pick buffers<cr>', {desc = 'Search open files'})
+vim.keymap.set('n', '<leader>ff', '<cmd>Pick files<cr>', {desc = 'Search all files'})
+vim.keymap.set('n', '<leader>fg', '<cmd>Pick grep_live<cr>', {desc = 'Search in project'})
+vim.keymap.set('n', '<leader>fd', '<cmd>Pick diagnostic<cr>', {desc = 'Search diagnostics'})
+vim.keymap.set('n', '<leader>fs', '<cmd>Pick buf_lines<cr>', {desc = 'Buffer local search'})
+
+-- See :help MiniStatusline.config
+require('mini.statusline').setup({})
+
+-- See :help MiniExtra
+require('mini.extra').setup({})
+
+-- See :help MiniSnippets.config
+require('mini.snippets').setup({})
+
+-- See :help MiniCompletion.config
+require('mini.completion').setup({
+  lsp_completion = {
+    source_func = 'omnifunc',
+    auto_setup = false,
+  },
+})
+
+require('conform').setup({
+  formatters_by_ft = {
+    python = { 'ruff_fix', 'ruff_format', 'ruff_organize_imports' },
+  },
+  format_on_save = {
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
+})
+
+-- LSP setup
+vim.api.nvim_create_autocmd('LspAttach', {
+  desc = 'LSP actions',
+  callback = function(event)
+    local opts = {buffer = event.buf}
+    vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+    vim.keymap.set('n', 'grd', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+
+    -- Use conform for formatting
+    vim.keymap.set({'n', 'x'}, 'gq', function()
+      require('conform').format({ async = true, lsp_fallback = true })
+    end, opts)
+
+    -- Diagnostic keymaps
+    vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
+    vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>', opts)
+    vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>', opts)
+
+    local id = vim.tbl_get(event, 'data', 'client_id')
+    local client = id and vim.lsp.get_client_by_id(id)
+
+    if client and client:supports_method('textDocument/completion') then
+      vim.bo[event.buf].omnifunc = 'v:lua.MiniCompletion.completefunc_lsp'
+    end
+  end,
+})
+
+-- ========================================================================== --
+-- ==                           LSP SERVERS                                == --
+-- ========================================================================== --
+-- Python: pyright
+-- uv tool install pyright
+vim.lsp.enable('pyright', {
+  settings = {
+    pyright = {
+      disableOrganizeImports = true,
+    },
+    python = {
+      analysis = {
+        ignore = {'*'},  -- Ruff handles diagnostics
+      }
+    }
+  }
+})
+
+-- Python: ruff for linting and formatting
+-- uv tool install ruff
+vim.lsp.enable('ruff')
